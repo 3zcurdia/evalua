b6fe5a492d87525715dede295b48fa92519a01af
Luis Ezcurdia Razo <ing.ezcurdia@gmail.com>
2020-03-06 17:52:18 -0600

    Add Bulma CSS framework

---
+++ config/initializers/omniauth.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+OmniAuth.config.logger = Rails.logger
+
+Rails.application.config.middleware.use OmniAuth::Builder do
+  provider :github, ENV['GITHUB_KEY'], ENV['GITHUB_SECRET']
+end
