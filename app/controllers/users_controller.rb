32b369610ff512bcc86512f23bd4988eb951ae68
Luis Ezcurdia Razo <ing.ezcurdia@gmail.com>
2020-03-15 01:29:16 -0600

    Apply linter

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,23 +1,16 @@
-class UsersController < ApplicationController
-  before_action :set_user, only: [:show, :edit, :update, :destroy]
+# frozen_string_literal: true
 
-  # GET /users
-  # GET /users.json
+class UsersController < ApplicationController
+  before_action :set_user, only: %i[show edit update destroy]
+
   def index
     @users = User.all
   end
 
-  # GET /users/1
-  # GET /users/1.json
-  def show
-  end
+  def show; end
 
-  # GET /users/1/edit
-  def edit
-  end
+  def edit; end
 
-  # PATCH/PUT /users/1
-  # PATCH/PUT /users/1.json
   def update
     if @user.update(user_params)
       redirect_to @user, notice: 'User was successfully updated.'
@@ -26,21 +19,18 @@ class UsersController < ApplicationController
     end
   end
 
-  # DELETE /users/1
-  # DELETE /users/1.json
   def destroy
     @user.destroy
     redirect_to users_url, notice: 'User was successfully destroyed.'
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_user
-      @user = User.find(params[:id])
-    end
 
-    # Only allow a list of trusted parameters through.
-    def user_params
-      params.require(:user).permit(:name, :role)
-    end
+  def set_user
+    @user = User.find(params[:id])
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :role)
+  end
 end
--- app/helpers/dashboard_helper.rb
+++ app/helpers/dashboard_helper.rb
@@ -1,9 +1,7 @@
 # frozen_string_literal: true
 
 module DashboardHelper
-  def active_menu_class(controller, action=nil)
-    if controller_name == controller
-      'is-active'
-    end
+  def active_menu_class(controller, _action = nil)
+    'is-active' if controller_name == controller
   end
 end
