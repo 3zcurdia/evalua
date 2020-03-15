83df0d1b68a0c2cd04d7ea848d275addbb47d545
Luis Ezcurdia Razo <ing.ezcurdia@gmail.com>
2020-03-14 23:37:50 -0600

    Improve destroy button access

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,19 +3,19 @@
 module ApplicationHelper
   def link_to_add(path:, label: nil, **options)
     link_to(path, options) do
-      "<span class='icon'><i class='fa fa-plus-square'></i></span> #{label}".html_safe
+      "<span class='icon is-small'><i class='fa fa-plus-square'></i></span> #{label}".html_safe
     end
   end
 
   def link_to_edit(path:, label: nil, **options)
     link_to(path, options) do
-      "<span class='icon'><i class='fa fa-edit'></i></span> #{label}".html_safe
+      "<span class='icon is-small'><i class='fa fa-edit'></i></span> #{label}".html_safe
     end
   end
 
   def link_to_destroy(path:, label: nil, **options)
     link_to(path, default_delete_options.merge(options)) do
-      "<span class='icon'><i class='fa fa-trash'></i></span> #{label}".html_safe
+      "<span class='icon is-small'><i class='fa fa-trash'></i></span> #{label}".html_safe
     end
   end
 
