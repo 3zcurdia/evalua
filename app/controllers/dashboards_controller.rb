fb963a6c417c0d8ed242e421af26f21bdcfcd51f
Luis Ezcurdia Razo <ing.ezcurdia@gmail.com>
2020-03-15 04:41:47 -0600

    Add counters to dashboard

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,5 +1,10 @@
 # frozen_string_literal: true
 
 class DashboardsController < ApplicationController
-  def show; end
+  def show
+    @rubric_item_count = RubricItem.count
+    @evaluation_count = Evaluation.count
+    @user_evaluation_count = UserEvaluation.count
+    @user_count = User.count
+  end
 end
